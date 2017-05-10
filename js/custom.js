69dc83f3c6bbbab69dc6014e21212c38c46bc05d
parvathyvd <parvathyvd@gmail.com>
2017-05-09 21:44:18 -0700

    My first angular js file

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,33 @@
 
 
-$(function() {
-    $('.my-counting').countdown({
-        date: "June 7, 2027 15:03:26"
-    });
+$(function () {
+
+  $("#step1").hide();
+  $("#step2").hide();
+
+  $("#bttnstep1").click(function()
+{
+  $("#step1").show();
+  $("#step2").hide();
+
+});
+
+$("#bttnstep2").click(function()
+{
+$("#step1").hide();
+$("#step2").show();
+
+});
+});
+
+console.log($("h1").attr("ng-reply"));
+
+
+
+var myApp = angular.module('myApp',[]);
+
+myApp.controller('mainController',function()
+{
+
+
 });
